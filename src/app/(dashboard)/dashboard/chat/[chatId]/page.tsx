7810dd1650bf0bb235b3ca77e36ae89f0cef5299
Marcos Maio <120725100+MarcosMaio@users.noreply.github.com>
2023-12-08 10:56:29 -0300

    feat: add props to Message component

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -52,6 +52,7 @@ const Page = async ({ params }: PageProps) => {
   const chatPartnerId = user.id === userId1 ? userId2 : userId1;
   const chatPartner = (await db.get(`user:${chatPartnerId}`)) as User;
   const initialMessages = await getChatMessages(chatId);
+
   return (
     <div className="flex-1 justify-between flex flex-col h-full max-h-[calc(100vh-6rem)]">
       <div className="flex sm:items-center justify-between py-3 border-b-2 border-gray-200">
@@ -80,7 +81,12 @@ const Page = async ({ params }: PageProps) => {
         </div>
       </div>
 
-      <Messages sessionId={session.user.id} initialMessages={initialMessages} />
+      <Messages
+        sessionImg={session.user.image}
+        chatPartner={chatPartner}
+        sessionId={session.user.id}
+        initialMessages={initialMessages}
+      />
       <ChatInput chatId={chatId} chatPartner={chatPartner} />
     </div>
   );
